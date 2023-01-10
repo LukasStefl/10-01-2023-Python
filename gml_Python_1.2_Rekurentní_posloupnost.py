6aaf7a203c276be8f1dd96ffdc68ccb226671d50
LukasStefl <lukas.stefl@student.ossp.cz>
2023-01-10 09:58:24 +0100

    commit

---
+++ gml_Python_1.2_Rekurentní_posloupnost.py
@@ -0,0 +1,9 @@
+def rekurentni_posloupnost(n):
+    if n == 0:
+        return []
+    elif n == 1:
+        return [2]
+    else:
+        L = rekurentni_posloupnost(n-1)
+        L.append(3*L[-1])
+        return L
