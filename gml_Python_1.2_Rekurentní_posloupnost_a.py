a64bf720be8c8ab477fbef27fd3c5e9b10449545
LukasStefl <lukas.stefl@student.ossp.cz>
2023-01-10 11:04:28 +0100

    commit

--- gml_Python_1.2_Rekurentní_posloupnost_a.py
+++ gml_Python_1.2_Rekurentní_posloupnost_a.py
@@ -1,11 +1,11 @@
-def rekurentni_posloupnost(n):
+def recursive_sequence(n):
     if n == 0:
         return []
     elif n == 1:
         return [2]
     else:
-        L = rekurentni_posloupnost(n-1)
+        L = recursive_sequence(n-1)
         L.append(3*L[-1])
         return L
 
-print(rekurentni_posloupnost(5))
+print(recursive_sequence(5))
--- gml_Python_1.2_Rekurentní_posloupnost_b.py
+++ gml_Python_1.2_Rekurentní_posloupnost_b.py
@@ -1,12 +1,12 @@
-def rekurentni_posloupnost(n):
+def recursive_sequence(n):
     if n == 0:
         return []
     elif n == 1:
         return [1]
     else:
-        L = rekurentni_posloupnost(n-1)
+        L = recursive_sequence(n-1)
         L.append(1/(1+L[-1]))
         return L
 
-        
-print(rekurentni_posloupnost(5))
+
+print(recursive_sequence(5))
