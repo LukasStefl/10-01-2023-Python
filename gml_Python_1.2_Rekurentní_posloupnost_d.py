b8743643ddf6e9c30f428f9800a4bcba066a14d9
LukasStefl <lukas.stefl@student.ossp.cz>
2023-01-10 11:07:03 +0100

    commit

--- gml_Python_1.2_Rekurentní_posloupnost_d.py
+++ gml_Python_1.2_Rekurentní_posloupnost_d.py
@@ -4,7 +4,7 @@ def recursive_sequence(n):
     elif n == 2:
         return 1
     else:
-        return 2*recursive_sequence(n-1) + recursive_sequence(n-2) - 3*recursive_sequence(n-2)
+        return 2*recursive_sequence(n-1) - 3*recursive_sequence(n-2)
 
-sequence = recursive_sequence(5)
+sequence = [recursive_sequence(i) for i in range(1,6)]
 print(sequence)
